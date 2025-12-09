9cf70f70c8aac0e94d0dabfef3dc279bdca734db
Dalibor Drevojanek <drevojanekdalibor@gmail.com>
2025-12-09 19:21:01 +0100

    cleanup

--- day_3/main.pl
+++ day_3/main.pl
@@ -2,20 +2,15 @@ $path = "input.txt";
 open(DATA, "<$path") or die "Couldn't open file $path, $!";
 
 $result = 0;
-while(<DATA>) {
-    $line = "$_";
+while(my $line = <DATA>) {
     $max = 0;
     $len = length($line);
     for ($i = 0; $i < $len - 2; $i = $i + 1) {
-        $first = substr($line, $i, 1);
         for ($j = $i + 1; $j < $len - 1; $j = $j + 1) {
-            $num = int($first . substr($line, $j, 1));
-            if ($num > $max) {
-                $max = $num;
-            }
+            $num = int(substr($line, $i, 1) . substr($line, $j, 1));
+            $max = $num > $max ? $num : $max;
         }
     }
     $result = $result + $max;
-    $max = 0;
 }
 print "$result\n"
